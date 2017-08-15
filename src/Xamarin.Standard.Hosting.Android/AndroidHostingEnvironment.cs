6c4cdb80a758a6de38b8b2f5f190e3a5b0581156
Darrell Tunnell <darrell.tunnell@reach-contact.com>
2017-08-15 13:31:22 +0100

    Fixed bug with contentroot path null.

--- src/Xamarin.Standard.Hosting.Android/AndroidHostingEnvironment.cs
+++ src/Xamarin.Standard.Hosting.Android/AndroidHostingEnvironment.cs
@@ -11,12 +11,10 @@ namespace Xamarin.Standard.Hosting.Android
 
         public AndroidHostingEnvironment(string appName, string contentRootPath)
         {
-            //hostEnv.ContentRootPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            this.ContentRootPath = contentRootPath;
             var fp = new NonWatchingPhysicalFileProvider(contentRootPath);
-            this.ContentRootFileProvider = fp;
-            // var currentContext = context.CurrentContext;
-            this.ApplicationName = appName;
-            // services.AddSingleton<IHostingEnvironment>(hostEnv);
+            this.ContentRootFileProvider = fp;          
+            this.ApplicationName = appName;          
         }
 
 
